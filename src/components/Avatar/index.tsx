b0c6dfebf7b0cc6f19cb5419a8138a8e54129309
Matheus Oliveira <matheuseu41@gmail.com>
2023-04-25 15:52:06 -0300

    estilizando a Sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/matheuz0liveira.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/matheuz0liveira.png" alt="" />
-    <Titulo fontSize={20}>Matheus</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Matheus Oliveira</Titulo>
+      <Paragrafo fontSize={14} tipo="secundario">
+        matheuz0liveira
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full-Stack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
