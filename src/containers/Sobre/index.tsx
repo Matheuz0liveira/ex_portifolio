51cc6ae3a5efeb7370c9297c68962da49d8fcdcc
Matheus Oliveira <matheuseu41@gmail.com>
2023-04-25 15:27:29 -0300

    criando a seção de Projetos e o componente

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -12,8 +12,14 @@ const Sobre = () => (
       Voluptatibus, aliquid?
     </Paragrafo>
     <GithubSecao>
-      <img src="https://github-readme-stats.vercel.app/api?username=matheuz0liveira&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
-      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=matheuz0liveira&layout=compact&langs_count=7&theme=dracula" />
+      <img
+        alt="Grafico gihub"
+        src="https://github-readme-stats.vercel.app/api?username=matheuz0liveira&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"
+      />
+      <img
+        alt="Grafico Github"
+        src="https://github-readme-stats.vercel.app/api/top-langs/?username=matheuz0liveira&layout=compact&langs_count=7&theme=dracula"
+      />
     </GithubSecao>
   </section>
 )
